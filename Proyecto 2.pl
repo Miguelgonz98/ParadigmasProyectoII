83e51c80d51b7880e75109c13f4abf6256f26d2b
Miguelgonz98 <mgonzalex236@gmail.com>
2021-11-13 16:31:12 -0600

    Montando el git

---
+++ Proyecto 2.pl
@@ -0,0 +1,17 @@
+%
+% ===============================================
+%
+% (c) 2021
+% version 1.0.0 2021-11-19
+%
+% -----------------------------------------------
+% EIF400 Paradigmas de Programación
+% 2do ciclo 2021, grupo 01
+% Proyecto 2
+%
+% A00148103 González Carvajal, Miguel GRUPO 3PM
+% 117620480 González Quesada, Arnoldo GRUPO 3PM
+% 117960315 Ocampo Marín, Victor      GRUPO 3PM
+%
+% ===============================================
+% inserte aquí el código místico xd
